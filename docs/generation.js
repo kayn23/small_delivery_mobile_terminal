98597b0ff1c1c47d9bae517e3452d7ea63204396
Anatoly Chernyshev <kayn23@yandex.ru>
2024-12-21 04:10:36 +0300

    docs

---
+++ docs/generation.js
@@ -0,0 +1,336 @@
+const pvz = {
+  vladimirPVZ: [
+    {
+      id: 18,
+      name: "ОЗОН",
+      address: "Судогодское шоссе 1",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:56:04.089Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 17,
+      name: "ОЗОН",
+      address: "Батурина 20",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:55:42.433Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 16,
+      name: "ОЗОН",
+      address: "Горького 44",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:55:30.773Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 15,
+      name: "ОЗОН",
+      address: "Улица Полины Осипенко 21",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:55:17.375Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 14,
+      name: "ОЗОН",
+      address: "проспект строителей 20б",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:55:02.721Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 13,
+      name: "ОЗОН",
+      address: "Студенческая 6д",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:54:45.452Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 12,
+      name: "ОЗОН",
+      address: "Большая нижегородская 19",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:54:26.498Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 11,
+      name: "ОЗОН",
+      address: "Октябрьский проспект 7",
+      cityId: 497,
+      acitve: true,
+      createdAt: "2024-12-20T23:54:10.885Z",
+      city: {
+        id: 497,
+        name: "Владимир",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+  ],
+  muromPVZ: [
+    {
+      id: 10,
+      name: 'ПВЗ "OZON"',
+      address: "Юбилейная ул, 58",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:03:09.034Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 9,
+      name: 'ПВЗ "OZON"',
+      address: "Кленовая ул, 32а",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:02:59.780Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 8,
+      name: 'ПВЗ "OZON"',
+      address: "ул. Щербакова, 19",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:02:38.178Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 7,
+      name: 'ПВЗ "OZON"',
+      address: "Комсомольская ул, 39А",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:02:08.113Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 6,
+      name: 'ПВЗ "OZON"',
+      address: "Московская ул, 32",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:01:55.175Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 5,
+      name: 'ПВЗ "OZON"',
+      address: "ул. Воровского, 22",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:01:03.699Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 4,
+      name: 'ПВЗ "OZON"',
+      address: "ул. Льва Толстова, 79",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:00:51.051Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 3,
+      name: 'ПВЗ "OZON"',
+      address: "ул. Войкова, 2А",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-11-21T22:00:30.043Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+    {
+      id: 1,
+      name: 'ПВЗ "Озон"',
+      address: "Войкова 4а",
+      cityId: 512,
+      acitve: true,
+      createdAt: "2024-10-24T18:07:13.976Z",
+      city: {
+        id: 512,
+        name: "Муром",
+        createdAt: "2024-10-24T18:07:13.976Z",
+      },
+    },
+  ],
+};
+const clients = [
+  {
+    id: 4,
+    email: "testCoutier@gmail.com",
+    roleId: 3,
+    name: null,
+    surname: null,
+    lastname: null,
+    documentNumber: null,
+    createdAt: "2024-10-31T18:26:59.319Z",
+    role: {
+      id: 3,
+      name: "courier",
+      createdAt: "2024-10-24T18:07:13.976Z",
+    },
+  },
+  {
+    id: 5,
+    email: "testCoutier2@gmail.com",
+    roleId: 3,
+    name: null,
+    surname: null,
+    lastname: null,
+    documentNumber: null,
+    createdAt: "2024-10-31T18:26:59.319Z",
+    role: {
+      id: 3,
+      name: "courier",
+      createdAt: "2024-10-24T18:07:13.976Z",
+    },
+  },
+  {
+    id: 3,
+    email: "test@gmail.com",
+    roleId: 1,
+    name: null,
+    surname: null,
+    lastname: null,
+    documentNumber: null,
+    createdAt: "2024-10-26T21:00:35.435Z",
+    role: {
+      id: 1,
+      name: "client",
+      createdAt: "2024-10-24T18:07:13.976Z",
+    },
+  },
+  {
+    id: 2,
+    email: "alex2@gmail.com",
+    roleId: 1,
+    name: null,
+    surname: null,
+    lastname: null,
+    documentNumber: null,
+    createdAt: "2024-10-24T20:36:48.671Z",
+    role: {
+      id: 1,
+      name: "client",
+      createdAt: "2024-10-24T18:07:13.976Z",
+    },
+  },
+  {
+    id: 1,
+    email: "admin@gmail.com",
+    roleId: 2,
+    name: null,
+    surname: null,
+    lastname: null,
+    documentNumber: null,
+    createdAt: "2024-10-24T18:07:13.976Z",
+    role: {
+      id: 2,
+      name: "admin",
+      createdAt: "2024-10-24T18:07:13.976Z",
+    },
+  },
+];
+
+const generateInvoice = (pvz, clients) => {
+  const stockId = getRandomPvz(pvz).id;
+  const senderId = getRandomClient(clients).id;
+  const recipientId = getRandomClient(
+    clients.filter(({ id }) => id !== senderId)
+  ).id;
+  return {
+    senderId,
+    recipientId,
+    stockId,
+  };
+};
+
+const getRandomPvz = (pvz) => {
+  const arr = Object.values(pvz).flat(Infinity);
+  return arr[getRandomInt(0, arr.length)];
+};
+
+const getRandomClient = (clients) => {
+  return clients[getRandomInt(0, clients.length)];
+};
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // Максимум не включается, минимум включается
+}
+
+"slovo".split("").forEach(() => {
+  console.log(JSON.stringify(generateInvoice(pvz, clients)));
+});
